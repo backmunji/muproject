a2d42736262bbdaac722fa14dc28d6115295b4fd
backmandu <bmj3222@naver.com>
2023-11-15 18:55:26 +0900

    다음은 마이페이지

--- src/Main/Header.js
+++ src/Main/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css'; 
 import LoginForm from './LoginForm';
 import SignUpForm from './SignUpForms';
@@ -6,6 +6,16 @@ import SignUpForm from './SignUpForms';
 const Header = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isLoginForm, setIsLoginForm] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [nickname, setNickname] = useState('');
+
+  useEffect(() => {
+    const storedNickname = localStorage.getItem('nickname');
+    if (storedNickname) {
+      setIsLoggedIn(true);
+      setNickname(storedNickname);
+    }
+  }, []);
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
@@ -15,23 +25,37 @@ const Header = () => {
     setIsLoginForm(!isLoginForm);
   };
 
-  
+  const handleLogout = () => {
+    // Clear user information from local storage
+    localStorage.removeItem('username');
+    localStorage.removeItem('nickname');
+    localStorage.removeItem('user_id');
+
+    // Update component state
+    setIsLoggedIn(false);
+    setNickname('');
+  };
+
   return (
     <div>
-      <header role="banner" className="header"> {/* 스타일 클래스를 적용합니다. */}
-        {/* Header 내용 */}
+      <header role="banner" className="header">
         <div>Logo</div>
-        <div>
-          <button className="cd-signin" onClick={() => toggleModal()}>
-            로그인
-          </button>
-          <button className="cd-signup" onClick={() => toggleModal()}>
-            회원가입
-          </button>
-        </div>
+        {isLoggedIn ? (
+          <div>
+            <p>{`${nickname}님 환영합니다!`}</p>
+            <button onClick={handleLogout}>로그아웃</button>
+          </div>
+        ) : (
+          <div>
+            <button className="cd-signin" onClick={() => toggleModal()}>
+              로그인
+            </button>
+            <button className="cd-signup" onClick={() => toggleModal()}>
+              회원가입
+            </button>
+          </div>
+        )}
       </header>
-
-      {/* 모달 */}
       {isModalOpen && (
         <div className="modal">
           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
